d1a124104747b52d07b678f3e16003ef18595b2d
Skydem <tomek.torbus@gmail.com>
2022-01-10 12:57:45 +0100

    dodano interfejs dla drugiej strony

--- interface.py
+++ interface.py
@@ -35,53 +35,50 @@ class Application (Frame):
         self.platnosc = Button(self.mainframe, text='Przejdź do płatności', pady=10, padx=20, command=self.page2).grid(row=5, column=0, sticky=S, columnspan=3)
 
     def page2(self):
-        
+        #new mainframe
         self.mainframe.destroy()
         self.mainframe = Frame(self.master)
         self.mainframe.grid(column=0, row=0, sticky=(W,E))
         
+        #labels
         self.title = Label(self.mainframe, text='Automat Biletowy MPK', font=('bold', 14), pady=20).grid(row=0, column=0, sticky=(N,E,W), columnspan=3)
-
-        self.tekst1 = Label(self.mainframe, text='Podsumowanie', pady=20).grid(row=1, column=0, sticky=W)
+        self.tekst1 = Label(self.mainframe, text='Podsumowanie', pady=20, font=(12)).grid(row=1, column=0, sticky=W)
 
         #ilosc biletow
-
+        self.bilet1 = Label(self.mainframe, text="20 minut ulgowy").grid(row=2,column=0, sticky=W)
+        self.bilet1_dodaj = Button(self.mainframe, text="+",padx=5).grid(row=2,column=1,sticky=W)
+        self.bilet1_usun = Button(self.mainframe, text="-",padx=5).grid(row=2,column=2,sticky=W)
         
-        self.p1_platnosc = Button(self.mainframe, text='Powrot', pady=10, padx=20, command=lambda:[self.mainframe.destroy(), self.page1()]).grid(row=5, column=0, sticky=S, columnspan=3)
+        #suma
+        self.suma_text = Label(self.mainframe, text="Łącznie: ").grid(row=3,column=0,sticky=W)
+        self.suma_val = 0
+        self.suma = Label(self.mainframe,text=(str(self.suma_val)+" zł")).grid(row=3,column=2,sticky=W)
+        self.wrzucono_val = 0
+        self.wrzucono_text = Label(self.mainframe,text="Wrzucono: ").grid(row=4,column=0,sticky=W)
+        self.wrzucono = Label(self.mainframe,text=(str(self.wrzucono_val)+" zł")).grid(row=4,column=2,sticky=W)
+        #wrzucanie monet
+        self.white_space = Label(self.mainframe, text=' ', pady=10).grid(row=5,column=0, sticky=W)
+        self.tekst2 = Label(self.mainframe, text='Wrzuć monety', font=(12)).grid(row=6, column=0, sticky=W)
+        
+        self.nazwy_monet = ["0.01 zł", "0.02 zł", "0.05 zł", "0.10 zł", "0.20 zł", "0.50 zł", "1.00 zł", "2.00 zł", "5.00 zł", "10.0 zł", "20.0 zł", "50.0 zł"]
+        row_moneta = 7
+        i=0
+        for moneta in self.nazwy_monet:
+            print("row = "+str(row_moneta)+", column= "+str(i)) #debug
+            Button(self.mainframe, text=moneta,padx=5).grid(row=row_moneta,column=i,sticky=(E, W),columnspan=1)
+            
+            i+=1
+            if(i==6):
+                row_moneta = 8
+                i=0
+
+
+        self.p1_platnosc = Button(self.mainframe, text='Powrot',pady=10, padx=20, command=lambda:[self.mainframe.destroy(), self.page1()]).grid(row=10, column=0, sticky=S, columnspan=3)
 
     
         
 
-        
 
 root = Tk()
 app = Application(master=root)
 app.mainloop()
-
-
-
-
-
-# window=Tk()
-# window.title("test")
-
-# mainframe = Frame(window)
-# mainframe.grid(column=0,row=0,sticky=W)
-
-# frame2 = Frame(window)
-# frame2.grid(column=1,row=0,sticky=E,padx=30)
-
-
-# button1 = Button(mainframe,text='kappa')
-# button1.grid(column=0,row=0)
-# button3 = Button(mainframe,text='POGGERS')
-# button3.grid(column=1,row=0)
-
-# button2 = Button(frame2,text='kappucino')
-# button2.grid(column=0,row=0)
-
-# # mainframe.destroy()
-
-# window.geometry("700x300")
-
-# window.mainloop()
