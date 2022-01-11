5a37060329ce2802da18f6ec7cba3b525722ff4b
Skydem <tomek.torbus@gmail.com>
2022-01-11 14:19:04 +0100

    dodano dodawanie monet

--- money_storage.py
+++ money_storage.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+from tempfile import NamedTemporaryFile
+import shutil
 
 class storage:
     def __init__(self):
@@ -18,10 +20,10 @@ class storage:
         except:
             print("Something went wrong")
             print(sys.exc_info()[:2])
+
     def add(self, value, count):
         try:
             self.money[value] += count
-            
         except KeyError:
             print("Nie istnieje taka moneta")
         except TypeError:
@@ -30,8 +32,29 @@ class storage:
             print(sys.exc_info()[:2])
 
 
+        try:
+            filename = "money.csv"
+            tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
+
+            with open(filename, 'r', newline='') as csvFile, tempfile:
+                #reader = csv.reader(csvFile, delimiter=',')
+                writer = csv.writer(tempfile, delimiter=',')
+
+                for key, value in self.money.items():
+                    writer.writerow({key, value})
+            
+            shutil.move(tempfile.name, filename)
+        except:
+            print('error!')
+            print(sys.exc_info()[:2])
+
+
 
 
 a = storage()
 a.add(1, 1)
+a.add(2, 2)
+a.add(5, 5)
+a.add(10, 10)
+a.add(20, 20)
 print(a.money)
